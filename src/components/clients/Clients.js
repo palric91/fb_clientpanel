435a831210a9178a53f75a3245b00c18ae5a7bfc
palric91 <palric91@gmail.com>
2018-09-19 22:07:08 +0200

    add | basic components

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "1234",
+        firstName: "Richko",
+        lastName: "Masta",
+        email: "richkomasta@gmail.com",
+        phone: "4772146",
+        balance: "99"
+      },
+      {
+        id: "2345",
+        firstName: "Szofcsi",
+        lastName: "Mastáné",
+        email: "zsofi@gmail.com",
+        phone: "1234567",
+        balance: "2000"
+      }
+    ];
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" /> Clients{" "}
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    return <h1>Loading...</h1>;
+  }
+}
+
+export default Clients;
